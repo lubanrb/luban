c87ca0afbcf340e6ddaa29608e3100d05334212c
Chi Man Lei <chi.lei@laxino.com>
2016-06-07 14:22:19 +0800

    Enhanced process management in controller

--- lib/luban/deployment/cli/service/controller.rb
+++ lib/luban/deployment/cli/service/controller.rb
@@ -2,11 +2,32 @@ module Luban
   module Deployment
     module Service
       class Controller < Worker
-        %i(process_stopped? process_started?
-           monitor_process unmonitor_process).each do |m|
-          define_method(m) do
-            raise NotImplementedError, "#{self.class.name}##{__method__} is an abstract method."
-          end
+        def pid
+          capture(:cat, "#{pid_file_path} 2>/dev/null")
+        end
+
+        def process_started?
+          !!process_grep.keys.first
+        end
+
+        def process_stopped?
+          !process_started?
+        end
+
+        def pid_file_orphaned?
+          process_stopped? and pid_file_exists?
+        end
+
+        def pid_file_missing?
+          process_started? and !pid_file_exists?
+        end
+
+        def pid_file_exists?
+          file?(pid_file_path, "-s") # file is NOT zero size
+        end
+
+        def process_pattern
+          raise NotImplementedError, "#{self.class.name}##{__method__} is an abstract method."
         end
 
         def start_process
@@ -17,9 +38,11 @@ module Luban
 
           output = start_process!
           if check_until { process_started? }
-            update_result "Successfully started #{package_full_name}: #{output}"
+            monitor_process
+            update_result "Start #{package_full_name}: [OK] #{output}"
           else
-            update_result "Failed to start #{package_full_name}: #{output}", 
+            remove_orphaned_pid_file
+            update_result "Start #{package_full_name}: [FAILED] #{output}", 
                           status: :failed, level: :error
           end
         end
@@ -30,11 +53,13 @@ module Luban
             return
           end
 
-          output = stop_process!
+          output = stop_process! || 'OK'
           if check_until { process_stopped? }
-            update_result "Successfully stopped #{package_full_name}: #{output}"
+            unmonitor_process
+            update_result "Stop #{package_full_name}: [OK] #{output}"
           else
-            update_result "Failed to stop #{package_full_name}: #{output}",
+            remove_orphaned_pid_file
+            update_result "Stop #{package_full_name}: [FAILED] #{output}",
                           status: :failed, level: :error
           end
         end
@@ -42,8 +67,12 @@ module Luban
         def restart_process
           if process_started?
             output = stop_process!
-            unless check_until { process_stopped? }
-              update_result "Failed to stop #{package_full_name}: #{output}",
+            if check_until { process_stopped? }
+              info "Stop #{package_full_name}: [OK] #{output}"
+              unmonitor_process
+            else
+              remove_orphaned_pid_file
+              update_result "Stop #{package_full_name}: [FAILED] #{output}",
                             status: :failed, level: :error
               return
             end
@@ -51,9 +80,11 @@ module Luban
 
           output = start_process!
           if check_until { process_started? }
-            update_result "Successfully restarted #{package_full_name}: #{output}"
+            update_result "Restart #{package_full_name}: [OK] #{output}"
+            monitor_process
           else
-            update_result "Failed to restart #{package_full_name}: #{output}", 
+            remove_orphaned_pid_file
+            update_result "Restart #{package_full_name}: [FAILED] #{output}", 
                           status: :failed, level: :error
           end
         end
@@ -65,9 +96,43 @@ module Luban
         def kill_process
           output = kill_process!
           if check_until { process_stopped? }
-            update_result "Successfully kill processs (#{output})."
+            unmonitor_process
+            remove_orphaned_pid_file
+            update_result "Kill #{package_full_name}: [OK] #{output}"
           else
-            update_result "Failed to kill process: #{output}"
+            update_result "Kill #{package_full_name}: [FAILED] #{output}"
+          end
+        end
+
+        def process_monitor_defined?
+          !process_monitor[:name].nil?
+        end
+
+        def monitor_process
+          if process_monitor_defined?
+            if monitor_process!
+              info "Turned on process monitor for #{service_entry}"
+            else
+              info "Failed to turn on process monitor for #{service_entry}"
+            end
+          end
+        end
+
+        def unmonitor_process
+          if process_monitor_defined?
+            if unmonitor_process!
+              info "Turned off process monitor for #{service_entry}"
+            else
+              info "Failed to turn off process monitor for #{service_entry}"
+            end
+          end
+        end
+
+        protected
+
+        %i(start_process! stop_process!).each do |m|
+          define_method(m) do
+            raise NotImplementedError, "#{self.class.name}##{__method__} is an abstract method."
           end
         end
 
@@ -80,18 +145,53 @@ module Luban
           succeeded
         end
 
-        protected
-
-        %i(start_process! stop_process! check_process!).each do |m|
-          define_method(m) do
-            raise NotImplementedError, "#{self.class.name}##{__method__} is an abstract method."
+        def check_process!
+          if pid_file_missing?
+            "#{package_full_name}: started but PID file does NOT exist - #{pid_file_path}"
+          elsif process_started?
+            "#{package_full_name}: started (PID #{pid})"
+          elsif pid_file_orphaned?
+            "#{package_full_name}: stopped but PID file exists - #{pid_file_path}"
+          else
+            "#{package_full_name}: stopped"
           end
         end
 
-        def kill_process!
-          pid = capture(pid_file_path)
-          output = capture(:kill, "-9 #{pid}")
-          output.empty? ? pid : output
+        def kill_process!(pattern = process_pattern)
+          capture(:pkill, "-9 -f \"#{pattern}\"")
+        end
+
+        def process_grep(pattern = process_pattern)
+          capture(:pgrep, "-l -f \"#{pattern}\" 2>/dev/null").split.inject({}) do |h, p|
+            pid, pname = p.split(' ', 2)
+            h[pid] = pname
+            h
+          end
+        end
+
+        def remove_orphaned_pid_file
+          rm(pid_file_path) if pid_file_orphaned?
+        end
+
+        def monitor_process!
+          test(process_monitor_command)
+        end
+
+        def unmonitor_process!
+          test(process_unmonitor_command)
+        end
+
+        def process_monitor_executable
+          @process_monitor_executable ||= env_path.join("#{stage}.#{process_monitor[:env]}").
+                                                   join('bin').join(process_monitor[:name])
+        end
+
+        def process_monitor_command
+          @process_monitor_command ||= "#{process_monitor_executable} monitor #{service_entry}"
+        end
+
+        def process_unmonitor_command
+          @process_unmonitor_command ||= "#{process_monitor_executable} unmonitor #{service_entry}"
         end
       end
     end
