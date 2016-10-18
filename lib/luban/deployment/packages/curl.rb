f9fadd93294b13f91c1d6884961950a848f8b143
Chi Man Lei <chi.lei@laxino.com>
2016-10-18 10:02:12 +0800

    Added package support for Curl

---
+++ lib/luban/deployment/packages/curl.rb
@@ -0,0 +1,39 @@
+module Luban
+  module Deployment
+    module Packages
+      class Curl < Luban::Deployment::Package::Base
+        apply_to :all do
+          before_install do
+            depend_on 'openssl', version: '1.0.2j'
+          end
+        end
+
+        class Installer < Luban::Deployment::Package::Installer
+          define_executable 'curl'
+
+          def source_repo
+            @source_repo ||= "https://curl.haxx.se"
+          end
+
+          def source_url_root
+            @source_url_root ||= "download"
+          end
+
+          def installed?
+            return false unless file?(curl_executable)
+            pattern = "curl #{package_version}"
+            match?("#{curl_executable} -V 2>&1", pattern)
+          end
+
+          def with_openssl_dir(dir)
+            if osx? 
+              @configure_opts << "--with-darwinssl"
+            else
+              @configure_opts << "--with-ssl=#{dir}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
