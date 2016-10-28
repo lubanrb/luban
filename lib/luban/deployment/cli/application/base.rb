84eca393f1b86b3a9aefb8de84d648ac792db3ad
Chi Man Lei <chi.lei@laxino.com>
2016-10-28 22:41:57 +0800

    Ensured only one controller from service or application controller can be executed

--- lib/luban/deployment/cli/application/base.rb
+++ lib/luban/deployment/cli/application/base.rb
@@ -197,8 +197,11 @@ module Luban
       %i(show_current show_summary).each do |action|
         define_method(action) do |args:, opts:|
           show_app_environment
-          send("#{action}_packages!", args: args, opts: opts)
-          send("#{action}_application", args: args, opts: opts) if has_source?
+          if has_services?
+            send("#{action}_packages!", args: args, opts: opts)
+          elsif has_source?
+            send("#{action}_application", args: args, opts: opts)
+          end
         end
         action_on_packages "#{action}_packages!", as: action
       end
